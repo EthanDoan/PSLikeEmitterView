a29a56575768909cbbe084291cc31d57fe99f9a4
Koichi Yamamoto <noughts@gmail.com>
2015-06-19 20:28:21 +0900

    a

---
+++ Pod/Classes/PSLikeEmitterView.h
@@ -0,0 +1,13 @@
+//
+//  PSLikeEmitterView.h
+//  Pods
+//
+//  Created by noughts on 2015/06/19.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSLikeEmitterView : UIView
+
+@end
---
+++ Pod/Classes/PSLikeEmitterView.m
@@ -0,0 +1,21 @@
+//
+//  PSLikeEmitterView.m
+//  Pods
+//
+//  Created by noughts on 2015/06/19.
+//
+//
+
+#import "PSLikeEmitterView.h"
+
+@implementation PSLikeEmitterView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
